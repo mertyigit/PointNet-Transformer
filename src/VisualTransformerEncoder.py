9fde3779f4334c50199a43de8b31c5dfbff5722a
mertyigit <mertyigitsengul@gmail.com>
2023-10-31 23:08:50 -0400

    v1 2D image for now

---
+++ src/VisualTransformerEncoder.py
@@ -0,0 +1,112 @@
+import os
+import sys
+import re
+from glob import glob
+import time
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchmetrics
+from torchmetrics.classification import MulticlassMatthewsCorrCoef
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from torch.nn.functional import kl_div
+from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import torchvision
+from torchvision.datasets.mnist import MNIST
+from torchvision.transforms import ToTensor
+
+import open3d as o3
+import math
+import yaml
+import argparse
+
+
+
+class VisualTransformerEncoderBlock(nn.Module):
+    '''
+    Not typical transformer block, the normalization and linear layers are there but the order is different.
+    '''
+    def __init__(self, hidden_d, n_heads, mlp_ratio=4):
+        super(VisualTransformerEncoderBlock, self).__init__()
+        self.hidden_d = hidden_d
+        self.n_heads = n_heads
+
+        self.norm1 = nn.LayerNorm(hidden_d)
+        self.mhsa = MultiHeadAttention(hidden_d, n_heads)
+        self.norm2 = nn.LayerNorm(hidden_d)
+        self.mlp = nn.Sequential(
+            nn.Linear(hidden_d, mlp_ratio * hidden_d),
+            nn.GELU(),
+            nn.Linear(mlp_ratio * hidden_d, hidden_d),
+        )
+
+    def forward(self, x):
+        out = x + self.mhsa(self.norm1(x), self.norm1(x), self.norm1(x)) 
+        out = out + self.mlp(self.norm2(out))
+        return out
+
+    
+class VisualTransformerEncoder(nn.Module):
+    def __init__(self, chw, n_patches=7, num_layers=2, hidden_d=8, n_heads=2):
+        # Super constructor
+        super(VisualTransformerEncoder, self).__init__()
+
+        # Attributes
+        self.chw = chw  # ( C , H , W )
+        self.n_patches = n_patches
+        self.num_layers = num_layers
+        self.n_heads = n_heads
+        self.hidden_d = hidden_d
+
+        # Input and patches sizes
+        assert (
+            chw[1] % n_patches == 0
+        ), "Input shape not entirely divisible by number of patches"
+        assert (
+            chw[2] % n_patches == 0
+        ), "Input shape not entirely divisible by number of patches"
+        self.patch_size = (chw[1] / n_patches, chw[2] / n_patches)
+
+        # 1) Linear mapper
+        self.input_d = int(chw[0] * self.patch_size[0] * self.patch_size[1])
+        self.linear_mapper = nn.Linear(self.input_d, self.hidden_d)
+
+        # 2) Learnable classification token
+        self.class_token = nn.Parameter(torch.rand(1, self.hidden_d))
+
+        # 3) Positional embedding
+        self.register_buffer(
+            "positional_embeddings",
+            get_positional_embeddings(n_patches**2, hidden_d),
+            persistent=False,
+        )
+
+        # 4) Transformer encoder blocks
+        self.blocks = nn.ModuleList(
+            [VisualTransformerEncoderBlock(hidden_d, n_heads) for _ in range(num_layers)]
+        )
+
+    def forward(self, images):
+        # Image to Patches
+        n, c, h, w = images.shape
+        patches = patchify(images, self.n_patches).to(self.positional_embeddings.device)
+
+        # Patch vector to Hidden Dimensions
+        tokens = self.linear_mapper(patches)
+
+        # Adding classification token to the tokens
+        #tokens = torch.cat((self.class_token.expand(n, 1, -1), tokens), dim=1)
+
+        # Adding positional embedding
+        out = tokens + self.positional_embeddings.repeat(n, 1, 1)
+
+        # Transformer Blocks
+        for block in self.blocks:
+            out = block(out)
+
+        return out, patches
---
+++ src/VisualTransformerGenerator.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import re
+from glob import glob
+import time
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchmetrics
+from torchmetrics.classification import MulticlassMatthewsCorrCoef
+from torch.utils.data import DataLoader
+import torch.optim as optim
+from torch.nn.functional import kl_div
+from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import torchvision
+from torchvision.datasets.mnist import MNIST
+from torchvision.transforms import ToTensor
+
+import open3d as o3
+import math
+import yaml
+import argparse
+
+class Transformer(nn.Module):
+    def __init__(self, hidden_d, n_heads, num_layers, d_ff, dropout, n_patches):
+        super(Transformer, self).__init__()
+
+        self.n_heads = n_heads
+        self.hidden_d = hidden_d
+        self.num_layers = num_layers
+        self.d_ff = d_ff
+        self.dropout = dropout
+        self.n_patches = n_patches
+
+        self.encoder = VisualTransformerEncoder((1, 28, 28), n_patches=self.n_patches, num_layers=self.num_layers, hidden_d=self.hidden_d, n_heads=self.n_heads)
+        self.decoder = VisualTransformerDecoder((1, 28, 28), d_ff=self.d_ff, dropout=self.dropout, n_patches=self.n_patches, num_layers=self.num_layers, hidden_d=self.hidden_d, n_heads=self.n_heads)
+
+        
+    def generate_mask(self, patches):
+        src = torch.ones((patches.size(0), patches.size(1)))
+        tgt = torch.ones((patches.size(0), patches.size(1)))
+
+        src_mask = (src != 0).unsqueeze(1).unsqueeze(2)
+        tgt_mask = (tgt != 0).unsqueeze(1).unsqueeze(3)
+        seq_length = tgt.size(1)
+        nopeak_mask = (1 - torch.triu(torch.ones(1, seq_length, seq_length), diagonal=1)).bool()
+        tgt_mask = tgt_mask & nopeak_mask
+
+        return src_mask, tgt_mask
+
+    def forward(self, images):
+        enc_output, patches = self.encoder(images)
+        src_mask, tgt_mask = self.generate_mask(patches)
+        src_mask = src_mask.to(device='mps')
+        tgt_mask = tgt_mask.to(device='mps')
+        dec_output = self.decoder(images, enc_output, src_mask, tgt_mask)
+        return dec_output
